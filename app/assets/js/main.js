8b0e40012db8ccdbabbdd2c6b5b0d04c6cf46298
yeison7654 <yeisoncarhuapoma@gmail.com>
2024-09-19 16:13:28 -0500

    estilos de la tablas echas

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,12 +1,14 @@
 const base_url = "http://localhost/sis-pid-books-isbmm";
 const url_logic = base_url + "/logic/";
 
-function alertas(data) {
+function alertas(data, timer = 1) {
     let alertMsj = document.querySelector(".alert");
     let danger = alertMsj.classList.contains("danger");
     let info = alertMsj.classList.contains("info");
     let success = alertMsj.classList.contains("success");
     let hidden = alertMsj.classList.contains("hidden");
+    document.querySelector(".alert-title").innerHTML = data.title
+    document.querySelector(".alert-text").innerHTML = data.text
     switch (data.type) {
         case "success":
             if (danger) {
@@ -48,4 +50,8 @@ function alertas(data) {
 
             break;
     }
+    let timeOut = timer * 1000
+    setTimeout(() => {
+        alertMsj.classList.toggle("hidden")
+    }, timeOut);
 }
